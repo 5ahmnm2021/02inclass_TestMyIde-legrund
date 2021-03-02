9a501e9e35c2cc4f3dd29f4257f9e03b7bdd89b1
legrund <lenag9802@gmail.com>
2021-03-02 09:12:32 +0100

    made changes to script

---
+++ 02inclass-TestMyIde-smeerws/Assets/MyGame/Scripts/TestMyIdeErrors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestMyIdeErrors : MonoBehaviour
+{
+    string Tag = "TestMyIdeErrors";
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    int a = 1;
+    int b = 2;
+    float d = 5;
+
+    TextMeshProUGUI TextPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int c = a + b;
+        Debug.Log("c =  3");
+        Debug.Log(Tag + "- Done ;)");
+    }
+
+    // Update is called once per frame
+    
+}
